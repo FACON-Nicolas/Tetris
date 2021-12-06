fc4f6186ce41afe7b69e39e1273057056943a8a2
FACON-Nicolas <84249866+FACON-Nicolas@users.noreply.github.com>
2021-12-06 20:25:11 +0100

    Update piece.py

--- source/piece.py
+++ source/piece.py
@@ -12,27 +12,35 @@ class Piece:
         self.tableau = tableau
 
     def initialiser_position(self):
-        self.x, self.y = 6, 0
+        self.x, self.y = -1, 0
         self.num_tableau = 0
-        
+
     def descendre(self):
         """ procedure permettant de faire descendre 
         la pièce du jeu actuellement en mouvement. 
         :todo :: voir pour faire descendre le tableau """
-        if self.y + 3 < 21: 
-            self.y +=1
+        self.y +=1
 
     def gauche(self):
         """ Procedure permettant à la piece en question d'aller
         a gauche si elle remplit la condition de la fonction """
-        self.x = self.x - 1 if self.x > 1 else self.x
+        self.x = self.x - 1 if self.x > 0 else self.x
     
     def droite(self):
         """ Procedure permettant à la piece en question d'aller
         a droite si elle remplit la condition de la fonction """
-        self.x = self.x + 1 if self.x + 3 > 21 else self.x
+        self.x = self.x + 1 if self.x + 3 < 12 else self.x
 
     def pivoter_piece(self):
         """ permet de passer au tableau suivant d'une piece permettant 
         à la classe 'Affichage' de simuler un pivotement de la piece"""
-        self.num_tableau = (self.tableau + 1) % 4
+        self.num_tableau = (self.num_tableau + 1) % 4
+
+    def get_num_tab(self):
+        return self.num_tableau
+
+    def get_tab(self):
+        return self.tableau
+
+    def get_tab_with_index(self):
+        return self.get_tab()[self.get_num_tab()]
